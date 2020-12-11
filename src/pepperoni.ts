0367dc9f5f15c78a8da61061de79de14224918fd
Steffi Au <steffi.au@unbounce.com>
2020-12-11 15:46:15 -0800

    peps texture

--- src/pepperoni.ts
+++ src/pepperoni.ts
@@ -1,7 +1,12 @@
 import * as THREE from 'three';
+const loader = new THREE.TextureLoader();
 
 const pepGeometry = new THREE.CylinderGeometry(2, 2, 0.1, 32);
-const pepMaterial = new THREE.MeshLambertMaterial({ color: 0x942525 });
+// const pepMaterial = new THREE.MeshLambertMaterial({ color: 0x942525 });
+const pepMaterial = new THREE.MeshBasicMaterial({
+  map: loader.load('./pepperoni.png'),
+});
+
 pepGeometry.scale(0.1, 0.1, 0.1);
 pepGeometry.center();
 
