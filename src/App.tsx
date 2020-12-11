033ae4d5b6434f14c55940436dbe5ade3ac6f7c6
Graham Madden <graham.madden@unbounce.com>
2020-12-11 09:48:15 -0800

    adds threejs example

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Hyperspace } from './Hyperspace'
 
 interface AppProps {}
 
-function App({}: AppProps) {
+export const App = ({}: AppProps) => {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hyperspace />
     </div>
   );
 }
